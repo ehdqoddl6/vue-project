f87c36f11a8512f54faff6e48a43acea27293c6c
ehdqoddl6 <ehdqoddl6@gmail.com>
2022-07-25 23:24:32 +0900

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
+      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
+    </h3>
+    <h4>{{ this.reverseMessage() }}</h4>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      message: "Hello World!",
+    };
+  },
+  methods: {
+    reverseMessage() {
+      return this.message.split("").reverse().join("");
+    },
+    notify() {
+      alert("navigation was prevented.");
+    },
+  },
+});
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
